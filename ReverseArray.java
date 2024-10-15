c874c5445a1dffeb88c3a75e9e262dce60d128d8
Atharv Kachare <104071489+Atharvkachare@users.noreply.github.com>
2024-10-15 19:59:09 +0530

    Add files via upload

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,33 +1,27 @@
-import java.util.Arrays;
-
-public class ReverseArray{
+import java.util.Scanner;
+public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,5};
-        int n=arr.length;
-       // reverseArray(arr); //Using Same array
-       reverseArray2(arr, 0,n-1); //Using recursion
-        System.out.println(Arrays.toString(arr));
-    }
-
-    // Using Same array
-    // static void reverseArray(int[] arr)
-    // {
-    //     int n =arr.length;
-    //     for(int i=0; i<n/2; i++)
-    //     {
-    //         int temp = arr[i];
-    //         arr[i] = arr[n-1-i];
-    //         arr[n-1-i] = temp;
-    //     }
-    // }
-
-     static void reverseArray2(int[] arr, int start, int end)
-    {
-        if(start>end)return;
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        reverseArray2(arr, start+1, end-1);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of array elements: ");
+        int n=sc.nextInt();
+        int []arr =  new int[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int res[] = new int[n];
+        int counter =0;
+        for(int i=n-1; i>=0; i--){
+            res[counter]=arr[i];
+            counter++;
+        }
+        System.out.println();
+        //Displaying array elements
+        for(int i=0; i<n; i++)
+        {
+            System.out.print(res[i] + " ");
+        }
+        sc.close();
+        
     }
 }
